79455570ee46cdcd19b4f9018496fff7ebcb9190
Nixieoffset <34421248+Nixieoffset@users.noreply.github.com>
2019-02-14 21:12:37 +0200

    Update master.lua

--- syreval/master.lua
+++ syreval/master.lua
@@ -1,12 +1,19 @@
--- **************/ SyrEval v.b31.1 \**************
+-- **************/ SyrEval v.b31.2 \**************
 
-mode = "30" -- pieejamie režīmi: @ = parastais, 30 = 30 sekunžu
-evalmap = [[]]
---         ^^ iekopē evaluējamo kodu šeit starpā
+mode = 30 -- pieejamie režīmi: -1 = parastais, 30 = 30 sekunžu
 
-tfm.exec.newGame(evalmap)
+eval1 = [[]] -- spied 1, lai palaistu šo mapi
+eval2 = [[]]
+eval3 = [[]]
+eval4 = [[]]
+eval5 = [[]]
+eval6 = [[]]
+--       ^^ iekopē evaluējamo kodu šeit starpā
+
+tfm.exec.newGame(eval1)
 tfm.exec.disableAutoNewGame(true)
 tfm.exec.disableAutoShaman(true)
+
 
 function eventChatCommand(name,cmd,n)
 if cmd:sub(1,1) == "a" then print("<N><b>[b]<VP>[color=#30ba76]Pieņemts[/color]</VP>[/b]</b>[color=#92cf91]<T> - būs pieejams "..cmd:sub(3).." versijā.</T>[/color]") end
@@ -16,19 +23,35 @@ end
 
 function eventNewPlayer(name)
 tfm.exec.bindKeyboard(name,18,true,true)
+tfm.exec.bindKeyboard(name,49,true,true) tfm.exec.bindKeyboard(name,50,true,true) tfm.exec.bindKeyboard(name,51,true,true) tfm.exec.bindKeyboard(name,52,true,true) tfm.exec.bindKeyboard(name,53,true,true) tfm.exec.bindKeyboard(name,54,true,true)
 end
 
 function eventKeyboard(name, key, down, x, y)
 if key==18 then
-tfm.exec.newGame(tfm.get.room.xmlMapInfo.xml) end 
+tfm.exec.newGame(tfm.get.room.xmlMapInfo.xml) 
 -- ^^ kad uzspiež alt, tiek pārlādēta mape
+
+elseif key==49 then
+tfm.exec.newGame(eval1)
+elseif key==50 then
+tfm.exec.newGame(eval2)
+elseif key==51 then
+tfm.exec.newGame(eval3)
+elseif key==52 then
+tfm.exec.newGame(eval4)
+elseif key==53 then
+tfm.exec.newGame(eval5)
+elseif key==54 then
+tfm.exec.newGame(eval6) end
 end
 
+
 function eventNewGame()
-if mode=="30" then
+if mode==30 then
 tfm.exec.setGameTime(30)
 end
 end
+
 
 for name,player in pairs(tfm.get.room.playerList) do eventNewPlayer(name) end
 -- ^^ definē 'name'
