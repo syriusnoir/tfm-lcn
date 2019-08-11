fbb3355849812e1742fa290c02ef452cd1f0c009
Nixieoffset <34421248+Nixieoffset@users.noreply.github.com>
2019-08-11 15:51:04 +0300

    Create v1.lua

---
+++ catch/v1.lua
@@ -0,0 +1,80 @@
+-- **************/ Ķereņu kods v1 - veidots uz syreval v.b42 kodola \**************
+
+mode = -1 -- pieejamie režīmi: -1 = parastais, 30 = 30 sekunžu
+
+eval = {[1] = [[<C><P F="8" L="2400" H="800" mgoc="-1" bh="" meta="Syrius#8114,K_1" portal="1080,660;1950,70"/><Z><S/><D/><O><O X="805" Y="585" C="4" P="0,0"/><O X="422" Y="600" C="4" P="0,0"/><O X="609" Y="567" C="4" P="-20,0"/><O X="613" Y="500" C="4" P="-160,0"/><O X="643" Y="464" C="4" P="-160,0"/><O X="797" Y="425" C="4" P="-230,0"/><O X="836" Y="455" C="4" P="-230,0"/><O X="869" Y="529" C="4" P="-180,0"/><O X="944" Y="376" C="7" P="0,0"/><O X="989" Y="426" C="45" P="90,0"/><O X="1209" Y="428" C="45" P="90,0"/><O X="989" Y="501" C="68" P="0,0"/><O X="905" Y="631" C="45" P="90,0"/><O X="1210" Y="528" C="45" P="90,0"/><O X="1293" Y="530" C="4" P="-30,0"/><O X="1300" Y="581" C="4" P="-30,0"/><O X="1215" Y="676" C="45" P="90,0"/><O X="1008" Y="748" C="68" P="0,0"/><O X="1071" Y="615" C="68" P="0,0"/><O X="1215" Y="762" C="68" P="0,0"/><O X="1207" Y="526" C="68" P="0,0"/><O X="1112" Y="679" C="68" P="0,0"/><O X="1105" Y="444" C="68" P="0,0"/><O X="305" Y="661" C="10" P="0,0"/><O X="268" Y="657" C="10" P="10,0"/><O X="228" Y="646" C="10" P="20,0"/><O X="187" Y="628" C="10" P="30,0"/><O X="152" Y="604" C="10" P="40,0"/><O X="120" Y="575" C="10" P="50,0"/><O X="94" Y="545" C="10" P="60,0"/><O X="77" Y="510" C="10" P="70,0"/><O X="66" Y="469" C="10" P="80,0"/><O X="63" Y="429" C="10" P="90,0"/><O X="307" Y="593" C="10" P="180,0"/><O X="271" Y="590" C="10" P="190,0"/><O X="238" Y="579" C="10" P="200,0"/><O X="207" Y="561" C="10" P="210,0"/><O X="175" Y="535" C="10" P="220,0"/><O X="149" Y="507" C="10" P="230,0"/><O X="131" Y="474" C="10" P="250,0"/><O X="128" Y="437" C="10" P="270,0"/><O X="371" Y="655" C="3" P="0,0"/><O X="333" Y="742" C="4" P="0,0"/><O X="408" Y="672" C="60" P="120,0"/><O X="300" Y="691" C="4" P="0,0"/><O X="133" Y="742" C="4" P="0,0"/><O X="19" Y="648" C="4" P="80,0"/><O X="2" Y="449" C="4" P="90,0"/><O X="62" Y="488" C="4" P="80,0"/><O X="162" Y="667" C="3" P="30,0"/><O X="90" Y="606" C="60" P="60,0"/><O X="115" Y="635" C="60" P="40,0"/><O X="53" Y="727" C="17" P="-10,0"/><O X="118" Y="330" C="4" P="-40,0"/><O X="76" Y="287" C="4" P="-40,0"/><O X="419" Y="465" C="4" P="0,0"/><O X="224" Y="442" C="4" P="13,0"/><O X="75" Y="388" C="10" P="-250,0"/><O X="102" Y="357" C="10" P="-220,0"/><O X="134" Y="330" C="10" P="-220,0"/><O X="167" Y="301" C="10" P="-220,0"/><O X="205" Y="279" C="10" P="-200,0"/><O X="244" Y="270" C="10" P="-190,0"/><O X="287" Y="269" C="10" P="-180,0"/><O X="241" Y="257" C="3" P="-10,0"/><O X="361" Y="312" C="4" P="40,0"/><O X="427" Y="307" C="4" P="40,0"/><O X="527" Y="396" C="3" P="48,0"/><O X="317" Y="287" C="10" P="220,0"/><O X="346" Y="310" C="10" P="220,0"/><O X="373" Y="333" C="10" P="220,0"/><O X="403" Y="358" C="10" P="220,0"/><O X="628" Y="659" C="3" P="30,0"/><O X="709" Y="706" C="3" P="30,0"/><O X="814" Y="656" C="3" P="-140,0"/><O X="729" Y="624" C="3" P="-180,0"/><O X="692" Y="605" C="60" P="-60,0"/><O X="229" Y="158" C="4" P="-40,0"/><O X="449" Y="245" C="4" P="0,0"/><O X="339" Y="134" C="4" P="-40,0"/><O X="360" Y="196" C="4" P="0,0"/><O X="643" Y="263" C="4" P="10,0"/><O X="820" Y="324" C="3" P="30,0"/><O X="759" Y="290" C="60" P="30,0"/><O X="344" Y="61" C="3" P="-40,0"/><O X="496" Y="120" C="4" P="30,0"/><O X="493" Y="51" C="4" P="30,0"/><O X="395" Y="17" C="60" P="-40,0"/><O X="510" Y="195" C="3" P="0,0"/><O X="605" Y="182" C="60" P="30,0"/><O X="603" Y="194" C="60" P="0,0"/><O X="573" Y="195" C="60" P="0,0"/><O X="668" Y="151" C="4" P="30,0"/><O X="640" Y="203" C="60" P="30,0"/><O X="258" Y="197" C="60" P="-30,0"/><O X="897" Y="174" C="67" P="-10,0"/><O X="1192" Y="173" C="67" P="10,0"/><O X="1293" Y="431" C="4" P="30,0"/><O X="1415" Y="621" C="4" P="70,0"/><O X="1461" Y="617" C="4" P="70,0"/><O X="1516" Y="411" C="4" P="-30,0"/><O X="1351" Y="410" C="4" P="30,0"/><O X="1340" Y="301" C="4" P="-40,0"/><O X="1355" Y="202" C="60" P="10,0"/><O X="1494" Y="173" C="4" P="-40,0"/><O X="1451" Y="141" C="4" P="-40,0"/><O X="1668" Y="106" C="4" P="0,0"/><O X="1669" Y="46" C="4" P="0,0"/><O X="1547" Y="60" C="60" P="-40,0"/><O X="1461" Y="748" C="3" P="-110,0"/><O X="1495" Y="711" C="60" P="70,0"/><O X="1621" Y="795" C="67" P="0,0"/><O X="1646" Y="730" C="67" P="0,0"/><O X="1690" Y="311" C="4" P="150,0"/><O X="1776" Y="216" C="45" P="90,0"/><O X="1777" Y="152" C="45" P="90,0"/><O X="1681" Y="374" C="4" P="-30,0"/><O X="1773" Y="368" C="45" P="90,0"/><O X="1774" Y="468" C="45" P="90,0"/><O X="1775" Y="568" C="45" P="90,0"/><O X="1869" Y="46" C="4" P="0,0"/><O X="2070" Y="45" C="4" P="0,0"/><O X="2166" Y="99" C="45" P="90,0"/><O X="2167" Y="199" C="45" P="90,0"/><O X="2167" Y="297" C="45" P="90,0"/><O X="2167" Y="397" C="45" P="90,0"/><O X="2168" Y="497" C="45" P="90,0"/><O X="2169" Y="597" C="45" P="90,0"/><O X="2138" Y="682" C="45" P="130,0"/><O X="2074" Y="757" C="45" P="130,0"/><O X="1842" Y="695" C="45" P="50,0"/><O X="1830" Y="731" C="3" P="0,0"/><O X="1948" Y="792" C="4" P="0,0"/><O X="1809" Y="793" C="3" P="0,0"/><O X="1777" Y="194" C="68" P="0,0"/><O X="1921" Y="81" C="68" P="0,0"/><O X="2133" Y="83" C="68" P="0,0"/><O X="2094" Y="150" C="68" P="0,0"/><O X="2168" Y="303" C="68" P="0,0"/><O X="2067" Y="408" C="68" P="0,0"/><O X="1775" Y="472" C="68" P="0,0"/><O X="1898" Y="356" C="68" P="0,0"/><O X="1907" Y="469" C="68" P="0,0"/><O X="2011" Y="534" C="68" P="0,0"/><O X="1971" Y="600" C="68" P="0,0"/><O X="2055" Y="601" C="68" P="0,0"/><O X="2015" Y="710" C="68" P="-180,0"/><O X="1715" Y="666" C="4" P="-4,0"/><O X="1681" Y="623" C="4" P="-4,0"/><O X="1464" Y="500" C="3" P="-30,0"/><O X="1523" Y="579" C="4" P="80,0"/><O X="1584" Y="675" C="3" P="-4,0"/><O X="1569" Y="539" C="4" P="80,0"/><O X="1574" Y="436" C="60" P="-30,0"/><O X="1046" Y="788" C="67" P="0,0"/><O X="532" Y="759" C="4" P="10,0"/><O X="468" Y="676" C="60" P="-70,0"/><O X="554" Y="714" C="4" P="10,0"/><O X="608" Y="708" C="3" P="27,0"/><O X="547" Y="676" C="3" P="27,0"/><O X="489" Y="663" C="1" P="0,0"/><O X="772" Y="782" C="67" P="2,0"/><O X="905" Y="683" C="45" P="90,0"/><O X="905" Y="660" C="68" P="0,0"/><O X="869" Y="685" C="60" P="0,0"/></O><L/></Z></C>]]}
+--       ^^ iekopē evaluējamo kodu šeit starpā
+
+tfm.exec.newGame(eval[1])
+tfm.exec.disableAutoNewGame(true)
+tfm.exec.disableAutoShaman(true)
+
+
+function eventChatCommand(name,cmd,n)
+
+    local c = { }
+    string.gsub(cmd, "%S+", function(arg)
+        c[#c + 1] = arg
+    end)
+
+if c[1] == "map" then tfm.exec.newGame(eval[tonumber(c[2])]) end
+if c[1] == "map*" then tfm.exec.newGame(eval[tonumber(c[2])],true) end
+end
+
+function gui()
+ui.addTextArea(32, "<font size='10'><p align='center'><CH2><a href='event:r'>[noraidīt]</a> <a href='event:al'>[pieņemt ar labojumiem]</a></CH2><D> <a href='event:ar'>[pieņemt kā rezerves]</a> <a href='event:arg'>[atstāt uz gaidīšanu]</a></D><CH> <a href='event:a'>[pieņemt]</a>", nil, 9, 380, 784, 27, 0x000022, 0x000022, 0.65, true)
+end
+
+function rmgui()
+ui.removeTextArea(32,nil) end
+
+function eventNewPlayer(name)
+tfm.exec.bindKeyboard(name,18,true,true)
+end
+
+function eventKeyboard(name, key, down, x, y)
+if key==18 then
+	tfm.exec.newGame(tfm.get.room.xmlMapInfo.xml) 
+end
+end
+
+
+function eventNewGame()
+if mode==30 then
+tfm.exec.setGameTime(30)
+end
+    local Ptag = string.match(tfm.get.room.xmlMapInfo.xml, "<P (.-)/>")
+    local meta = string.match(Ptag, 'meta ?= ?"(.-)"')
+    local mapName
+
+    if meta then
+        mapName = string.gsub(meta, ",", "<BL> - $", 1).."</BL>" end
+
+	    ui.setMapName(mapName)
+	if Ptag:find('portal') then
+		for value in Ptag:gmatch('portal="(.+)"') do
+			print(value)            
+			tfm.exec.addShamanObject(26,tonumber(value:match('(.-);'):match('(.-),')),tonumber(value:match('(.-);'):match(',(.+)')),0,0,0,false)
+			tfm.exec.addShamanObject(27,tonumber(value:match(';(.+)'):match('(.-),')),tonumber(value:match(';(.+)'):match(',(.+)')),0,0,0,false)    
+		end
+	end
+end
+
+function eventTextAreaCallback(textAreaID, playerName, callback)
+if callback=="a" then ui.addPopup(0, 2, "<p align='center'>Versija</p>", playerName, 581, 289, 100, true) end
+if callback=="r" then ui.addPopup(1, 2, "<p align='center'>Komentārs</p>", playerName, 581, 289, 200, true) end
+if callback=="al" then ui.addPopup(2, 2, "<p align='center'>Komentārs</p>", playerName, 581, 289, 200, true) end
+if callback=="ar" then ui.addPopup(3, 2, "<p align='center'>Komentārs</p>", playerName, 581, 289, 200, true) end 
+if callback=="arg" then ui.addPopup(4, 2, "<p align='center'>Komentārs</p>", playerName, 581, 289, 200, true) end 
+end
+
+function eventPopupAnswer(popupID, playerName, answer)
+if popupID==0 then print("<N><b>[b]<VP>[color=#30ba76]Pieņemts[/color]</VP>[/b]</b>[color=#92cf91]<T> - būs pieejams "..answer.." versijā.</T>[/color]") end  
+if popupID==1 then print("<N><b>[b]<font color='#eb1d51'>[color=#eb1d51]Nav pieņemts[/color]</font>[/b]</b><R> - "..answer) end
+if popupID==2 then print("<N><b>[b]<font color='#eb1d51'>[color=#eb1d51]Pieņemu ar labojumiem[/color]</font>[/b]</b><R> - "..answer) end
+if popupID==3 then print("<N><b>[b]<CE>[color=#e68d43]Pieņemts[/color]</CE>[/b]</b>[color=#f0a78e]<CEP> - būs pieejama kā rezerves mape! <font size='10'>[size=10]"..answer.."[/size]</font></CEP>[/color]") end
+if popupID==4 then print("<N><b>[b]<CE>[color=#e68d43]Pieņemts[/color]</CE>[/b]</b>[color=#f0a78e]<CEP> - atstāju rezerves rotācijā, līdz izlaboju dizainu. <font size='10'>[size=10]"..answer.."[/size]</font></CEP>[/color]") end
+end
+
+for name,player in pairs(tfm.get.room.playerList) do eventNewPlayer(name) end
+-- ^^ definē 'name'
